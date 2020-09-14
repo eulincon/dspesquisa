ec314ba1fb1dc4a7b0368fca274eac4e69cf8dd2
Lincon Brito <38511541+zlincon@users.noreply.github.com>
2020-09-14 13:54:37 -0300

    Initial commit

---
+++ backend/src/main/java/com/devsuperior/dspesquisaapi/DspesquisaApiApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dspesquisaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DspesquisaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DspesquisaApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/dspesquisaapi/DspesquisaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dspesquisaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DspesquisaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
